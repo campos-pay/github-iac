b629b0dee389a9080ddcbd7c608edf825437f2a6
Guerlielton <guerlielton@gmail.com>
2022-07-26 22:18:29 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "github_teams_members" {
   source       = "github.com/campos-pay/modulo-github.git"
   organization = "campos-pay"
-  team_members = ["guerlielton"]
+  team_members = ["guerlielton","guerlielton-campos"]
   team_name    = "devteam"
 }

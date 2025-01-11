3083e7daaa73c3690b0c5416b24c5667e594e6f3
guerlielton <guerlielton@gmail.com>
2025-01-11 17:26:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-module "github_teams_members" {
-  source       = "github.com/campos-pay/modulo-github.git"
-  organization = "campos-pay"
-  team_members = ["guerlielton","guerlielton-campos"]
-  team_name    = "devteam"
+#module "github_teams_members" {
+#  source       = "github.com/campos-pay/modulo-github.git"
+#  organization = "campos-pay"
+#  team_members = ["guerlielton","guerlielton-campos"]
+#  team_name    = "devteam"
+#}
+module "github_repo" {
+  source  = "git::https://github.com/campos-pay/terraform-github-module.git?ref=dev"
+  organization = "campos-pay"   
+  team_name = "devteam"
+  repo_name = ["new-repository"]
+  repo_description = "Description of the new repository"
 }

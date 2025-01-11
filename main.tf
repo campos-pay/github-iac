882a91b767cb8352247b6530de1d3794a1634d70
guerlielton <guerlielton@gmail.com>
2025-01-11 19:53:09 +0000

    chore: remove description repo

--- main.tf
+++ main.tf
@@ -5,10 +5,9 @@
 #  team_name    = "devteam"
 #}
 module "github_repo" {
-  source  = "git::https://github.com/campos-pay/devops-github-module.git?ref=dev"
-  organization = "campos-pay"   
-  team_name = "devteam"
-  repo_name = ["new-repository", "new-repository-1"]
-  repo_description = "Description of the new repository"
+  source       = "git::https://github.com/campos-pay/devops-github-module.git?ref=dev"
+  organization = "campos-pay"
+  team_name    = "devteam"
+  repo_name    = ["new-repository", "new-repository-1"]
   #protected_branches = ["dev"]
 }
